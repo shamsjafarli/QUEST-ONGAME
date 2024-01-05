a9f8bd2553a9bfb4ab32cce85539fd9d37ff9b28
Shams Jafarli <shams.jafarli.2021@gmail.com>
2024-01-05 21:09:52 +0300

    update

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const Questions = [
     {
         title: "Funny in advance?",
-        trueAnswer: "Hilarious",
-        answers: ["Hideous", "Hilarious", "Wealthy", "Ecstatic", "Frail"]
+        trueAnswer: "B.Hilarious",
+        answers: ["A.Hideous", "B.Hilarious", "C.Wealthy", "D.Ecstatic", "E.Frail"]
     },
     {
         title: "What is the meaning just in case?",
-        trueAnswer: "önlem",
-        answers: ["önlem", "harika", "hizli", "hoş", "bakimsiz"]
+        trueAnswer: "A.önlem",
+        answers: ["A.önlem", "B.harika", "C.hizli", "D.hoş", "E.bakimsiz"]
     },
     {
         title: "Swear translate turkish",
-        trueAnswer: "yemin ederim",
-        answers: ["yemin ederim", "bakariz", "bestelemek", "yalanci", "ima etmek"]
+        trueAnswer: "A.yemin ederim",
+        answers: ["A.yemin ederim", "B.bakariz", "C.bestelemek", "D.yalanci", "E.ima etmek"]
     },
     {
         title: "Solo what is the meaning?",
-        trueAnswer: "alone",
-        answers: ["hideous", "hilarious", "alone", "ecstatic", "frail"]
+        trueAnswer: "C.alone",
+        answers: ["A.hideous", "B.hilarious", "C.alone", "D.ecstatic", "E.frail"]
     },
     {
         title: " Dünya umrumda değil translate turkish",
-        trueAnswer: "can't be bothered",
-        answers: ["go with the flow", "can't be bothered", "don't be naive", "stop bugging me", "what's your game?"]
+        trueAnswer: "B.can`t be bothered",
+        answers: ["A.go with the flow", "B.can`t be bothered", "C.don't be naive", "D.stop bugging me", "E.what's your game?"]
     },
+    {
+        title: "Ben öncüyüm translate turkish?",
+        trueAnswer: "B.I am pioneer",
+        answers: ["A.You have not loyalty", "B.I am pioneer", "C.I am obsessed with", "D.I deserved to", "E.I am no longer "]
+    }
+
+
+
+
 ]
+
+
+
 
 
 class QuestionG {
@@ -40,9 +52,9 @@ class QuestionG {
     nextQuest() {
         if (this.nextQ == this.qData.length - 1) {
             console.log("oyun bitdi")
-            return false;
-        }
-        else {
+            gameEnd.textContent = "CONGRATULATİONS"
+            endSound.play()
+        } else {
             this.nextQ += 1;
             const qTime = this.qData[this.nextQ];
             this.currentQuestion = qTime;
@@ -60,6 +72,14 @@ const falseImage = document.querySelector("#falseImage");
 const point = document.querySelector("#point")
 const correctSound = document.querySelector("#correctSound");
 const wrongSound = document.querySelector("#wrongSound")
+const progress = document.querySelector("#progress")
+const gameEnd = document.querySelector("#gameEnd")
+const endSound = document.querySelector("#endSound")
+
+let correctAnswers = 0;
+
+
+
 
 const gameQ = new QuestionG(Questions);//!json adıdı bu
 
@@ -72,7 +92,7 @@ function startGame() {
     qBtn.innerHTML = qObj.answers
         .map(
             (item) =>
-                `<button class="btn btn-outline-primary"   onclick="selectItem('${item}')">A.${item}</button>`
+                `<button class="btn btn-outline-primary"   onclick="selectItem('${item}')">${item}</button>`
         ).join("");
 
     console.log(qObj);
@@ -92,10 +112,13 @@ function selectItem(userChoose) {
     if (userChoose === currentQuestion.trueAnswer) {
         console.log("düz tapdi");
 
+        correctAnswers++;
+        progress.style.width = `${(correctAnswers / Questions.length) * 100}%`;
         gameQ.point += 10;
         point.textContent = `Your point: ${gameQ.point}`;
-        falseImage.style.display = "none";
+
         okayİmg.style.display = "block";
+        falseImage.style.display = "none";
         correctSound.play();
 
         setTimeout(() => {
@@ -110,11 +133,12 @@ function selectItem(userChoose) {
         wrongSound.play()
         setTimeout(() => {
             startGame();
-            falseİmg.style.display = "none";
+            falseImage.style.display = "none";
         }, 1500);
     }
-}
 
+
+}
 
 
 
